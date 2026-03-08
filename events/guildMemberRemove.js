5b026c613ed326cbe540c99c2a5087d2c35ee8b2
FiiXhub <andiabdwahabzainal@gmail.com>
2026-03-08 22:40:15 +0700

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,6 +3,6 @@ const updateStats = require("../utils/updateStats");
 module.exports = {
   name: "guildMemberRemove",
   async execute(member) {
-    updateStats(member.guild);
+    await updateStats(member.guild);
   }
 };
